174abaa923b4c88b0ac0c450b7524bfed0b6c8d0
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-02-06 19:19:38 +0200

    some changes in Advanced_JS

---
+++ Advanced_JS/script.js
@@ -0,0 +1,85 @@
+// Exercises 1 - ECMAScript6
+//1
+// let user = {
+//     name: "John",
+//     years: 30
+// }
+// console.log(user.name)
+// console.log(user.years)
+// let adminCheck = user.isAdmin!==undefined;
+// console.log(adminCheck)
+
+//2
+// let ourPlanetName = "Earth";
+// let currentVisitor = "human";
+
+//3
+// let phrase = "Hello"
+//
+// if (true) {
+//     let user = "John";
+//     function sayHi() {
+//         alert(`${phrase}, ${user}`)
+//     }
+// }
+//
+// sayHi()
+
+// result will be 'Hello, John'
+
+//4
+// let user = {};
+// user.name = 'John';
+// user.surname = 'Smith';
+// user.name = 'Pete';
+// delete user.name;
+
+
+//5
+// const user = {
+//     name: "John"
+// }
+//
+// does it work?
+// user.name = "Pete"
+
+
+//6
+// let salaries = {
+//     Fred: 100,
+//     Ted: 160,
+//     Ghaith: 130
+// }
+// let result = 0;
+// for (let key in salaries){
+//     result += salaries[key];
+// }
+// console.log(result)
+
+
+//7
+// if (a + b < 4) {
+//     result = 'Below';
+// } else {
+//     result = 'Over';
+// }
+//
+// let result = a + b < 4 ? 'Below' : 'Over';
+
+
+//8
+// let message;
+//
+// if (login == 'Employee') {
+//     message = 'Hello';
+// } else if (login == 'Director') {
+//     message = 'Greetings';
+// } else if (login == '') {
+//     message = 'No login';
+// } else {
+//     message = '';
+// }
+
+// let message = login === 'Employee' ? 'Hello' : login === 'Director' ? 'Greetings' : login === '' ? 'No login' : '';
+
+

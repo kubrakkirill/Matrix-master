238eb5fb630868b12dd5fabfe41d4686453d4061
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-02-06 19:12:40 +0200

    Fix some code in JQuery_Assignment_1
    First Commit for JQuery_Assignment_2

--- JQuery_Assignment_1/script.js
+++ JQuery_Assignment_1/script.js
@@ -1,9 +1,9 @@
 $('#btnHide').click(function (){
     $('#paraHide').toggle()
-})
+});
 $('#btnAppend').click(function (){
     $('#paraAppend').append(' Some new text.')
-})
+});
 $('#btnColor').click(function (){
     $('#paraColor').css('color', 'red')
-})
+});

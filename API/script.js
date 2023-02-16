9d2f0f5eca9feab83a02cbfb8218c023f49ae1f2
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-02-16 13:21:00 +0100

    first commit for API

---
+++ API/script.js
@@ -0,0 +1,36 @@
+const btnSearch = document.querySelector('#search');
+const input = document.querySelector('#name');
+const result = document.querySelector('#result');
+const APIKEY = "AIzaSyCn8GYsV4Nr-YjieW4Dz84AHLu8I_qBueg";
+const ApiEndPoint = "https://tenor.googleapis.com/v2/search";
+
+btnSearch.addEventListener("click", function (event){
+
+    event.preventDefault();
+    const searchTerm = input.value.trim();
+
+    if (!searchTerm){
+        console.error("Error");
+        return;
+    }
+
+    const url = `${ApiEndPoint}?q=${searchTerm}&key=${APIKEY}&client_key=my_project&limit=8`;
+    console.log(url)
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status >= 200 && this.status <=300) {
+                const response = JSON.parse(this.responseText);
+                const gifs = response.result;
+                let output = "";
+                for (let i = 0; i < gifs.length; i++) {
+                    output += `<img src="${gifs[i].media[0].gif.url}" alt="${gifs[i].title}">`;
+                }
+                result.innerHTML = output;
+            } else {
+                console.error("There's an error you gonna need to wait a min " + this.status);
+            }
+        }
+    }
+});
